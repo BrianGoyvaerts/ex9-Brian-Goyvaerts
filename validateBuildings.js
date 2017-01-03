dd992d2dcd187016f3f1ce702f5dca5afdc719df
BrianGoyvaerts <r0595245@student.thomasmore.be>
2017-01-04 00:18:32 +0100

    mongoose!

---
+++ validateBuildings.js
@@ -0,0 +1,36 @@
+module.exports = {
+    checkvalues: function (list) {
+        var errors = 0;
+
+        if (list["BuildingID"] <= 0) {
+            console.log("BuildingID cannot be 0");
+            errors++;
+        }
+        if (typeof list["BuildingID"] !== "number") {
+            console.log("BuildingID must be a number");
+            errors++;
+        }
+
+
+        if (typeof list["Name"] !== "string") {
+            console.log("Name should be a string");
+            errors++;
+        }
+        if (typeof list["Name"] == "") {
+            console.log("Fill in the name");
+            errors++;
+        }
+        
+        
+        if (typeof list["City"] !== "string") {
+            console.log("City should be a string");
+            errors++;
+        }
+        if (typeof list["City"] == "") {
+            console.log("Name the city");
+            errors++;
+        }
+    return errors
+    }
+};
+
