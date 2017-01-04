24267718cb3a00500cf3b112aa842fabfadfd248
BrianGoyvaerts <r0595245@student.thomasmore.be>
2017-01-04 01:38:37 +0100

    updated LocationStorage

--- LocationStorage.js
+++ LocationStorage.js
@@ -1,19 +1,55 @@
-module.exports =  {
-  Locations : {},
-  
-  saveLocation : function(Location){
-    this.Location[Location.id]= Location;
-  },
-  listAllLocations : function(){
-    var rtnValue =[];
-    for (var item in this.Locations) {
-      rtnValue.push(this.Locations[item]);
-    };
-    return rtnValue;
-  },
-  findLocations : function(id){
-    return this.Locations[id];
-  }
+var mongoose = require("mongoose");
+
+var LocationSchema = mongoose.Schema({
+    LocationID: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Type: {
+        type: String,
+        required: true
+    },
+    Coordinate: {
+        type: String,
+        required: true
+    }, 
+    Floor: {
+        type: Number,
+        required: true
+    }, 
+    Capacity: {
+        type: Number,
+        required: true
+    }, 
+    BlockID: {
+        type: Number,
+        required: true
+    }, 
+    BuildingID: {
+        type: Number,
+        required: true
+    }
+});
+
+var Location1 = mongoose.model('Locations', LocationSchema);
+
+module.exports = {
+
+    listAllLocations: function (callback) {
+        Location1.find(callback);
+    },
+    findLocation: function (Coordinate, callback) {
+        Location1.find({Coordinate: Coordinate}, callback);
+    },
+    createLocation: function (Location, callback) {
+        Location1.create(Location, callback);
+    },
+    updateLocation: function (LocationID, newLocation, callback) {
+        Location1.update({LocationID: LocationID}, newLocation, callback);
+    }
 };
-
-
