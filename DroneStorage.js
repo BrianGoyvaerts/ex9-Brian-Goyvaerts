e18178756794fa990d9dba1b68c5c06ac35f7df0
BrianGoyvaerts <r0595245@student.thomasmore.be>
2017-01-04 02:15:43 +0100

    update Drones

--- DroneStorage.js
+++ DroneStorage.js
@@ -1,19 +1,54 @@
-module.exports =  {
-  Drones : {},
-  
-  saveDrone : function(Drone){
-    this.Drone[Drone.id]= Drone;
-  },
-  listAllDrones : function(){
-    var rtnValue =[];
-    for (var item in this.Drones) {
-      rtnValue.push(this.Drones[item]);
-    };
-    return rtnValue;
-  },
-  findDrones : function(id){
-    return this.Drones[id];
-  }
+var mongoose = require("mongoose");
+
+var DroneSchema = mongoose.Schema({
+    DroneID: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Mac_Address: {
+        type: String,
+        required: true
+    },
+    Last_Packet_Date: {
+        type: Date, 
+        required: true
+    }, 
+    LocationID: {
+        type: Number,
+        required: true, 
+        unique: true
+    }, 
+    BlockID: {
+        type: Number,
+        required: true, 
+        unique: true
+    }, 
+    BuildingID: {
+        type: Number,
+        required: true, 
+        unique: true
+    }
+ });
+
+var Drone1 = mongoose.model('Drones', DroneSchema);
+
+module.exports = {
+
+    listAllDrones: function (callback) {
+        Drone1.find(callback);
+    },
+    findDrone: function (DroneID, callback) {
+        Drone1.find({DroneID: DroneID}, callback);
+    },
+    createDrone: function (Drone, callback) {
+        Drone1.create(Drone, callback);
+    },
+    updateDrone: function (DroneID, newDrone, callback) {
+        Drone1.update({DroneID: DroneID}, newDrone, callback);
+    }
 };
-
-
